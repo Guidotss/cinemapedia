a840399dc73be2c01e863b5129bdc071101efe8f
Guidotss <guidoolguin@hotmail.com>
2024-02-14 13:51:49 -0300

    Mostrando informacion basica de peliculas en pantalla

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:cinemapedia/config/constants/env_variables.dart';
+import 'package:cinemapedia/presentation/screens/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String name = 'home_screen';
@@ -8,9 +10,37 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('TheMovieDb Key: ${EnvVariables.theMovieDbKey}'),
-      ),
+      body: _HomeView(),
     ); 
   }
-} 
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage(); 
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+
+    return ListView(
+      children: [
+        ...nowPlayingMovies.map((movie) => ListTile(
+          title: Text(movie.title),
+          subtitle: Text(movie.overview),
+        )).toList(),
+      ],
+    ); 
+  }
+}
