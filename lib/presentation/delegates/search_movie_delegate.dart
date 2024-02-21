9e82e900aa9dc495bc436416321708067253e712
Guidotss <guidoolguin@hotmail.com>
2024-02-21 19:48:52 -0300

    comenzando buildSuggestions en SearchMovieDelegate

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,28 +1,38 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+
+typedef SearchMoviesCallback = Future<List<Movie>>Function(String query); 
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
+
+  final SearchMoviesCallback searchMoviesCallback;
+  SearchMovieDelegate({required this.searchMoviesCallback}); 
+  
 
   @override
   String get searchFieldLabel => 'Buscar película';
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
-      IconButton(
-        onPressed: () {
-          query = '';
-        },
-        icon: const Icon(Icons.clear),
-      )
+        FadeIn(
+          animate: query.isNotEmpty,
+          duration: const Duration(milliseconds: 200),
+          child: IconButton(
+            onPressed: () {
+              query = '';
+            },
+            icon: const Icon(Icons.clear),
+          ),
+        )
     ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
     return IconButton(
-      onPressed: () {
-        close(context, null);
-      },
-      icon: const Icon(Icons.arrow_back),
+      onPressed: () => close(context, null), 
+      icon: const Icon(Icons.arrow_back_ios_new_outlined),
     );
   }
 
@@ -33,7 +43,21 @@ class SearchMovieDelegate extends SearchDelegate {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Container();
+    return FutureBuilder(
+      future: searchMoviesCallback(query),
+      initialData: const [],
+      builder: (context, snapshot) {
+        final movies = snapshot.data as List<Movie>;
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(movies[index].title),
+              onTap: () => close(context, movies[index]),
+            ); 
+          },
+        ); 
+      },
+    ); 
   }
-
 }
--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,11 +1,13 @@
 import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CustomAppBar extends StatelessWidget {    
+class CustomAppBar extends ConsumerWidget {    
   const CustomAppBar({super.key});    
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
     return SafeArea(
@@ -21,9 +23,12 @@ class CustomAppBar extends StatelessWidget {
               const Spacer(),
               IconButton(
                 onPressed: (){
+                  final movieRepository = ref.read(movieRepositoryProvider);
                   showSearch(
                     context: context, 
-                    delegate: SearchMovieDelegate()
+                    delegate: SearchMovieDelegate(
+                      searchMoviesCallback: movieRepository.searchMovies
+                    )
                   ); 
                 }, 
                 icon: const Icon(Icons.search),
